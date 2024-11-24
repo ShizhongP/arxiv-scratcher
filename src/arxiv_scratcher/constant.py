fb8ab6b05a007890eea10d91a30c081b6a40709c
psz <2422158541@qq.com>
2024-11-24 14:41:15 +0800

    fix

--- src/arxiv_scratcher/constant.py
+++ src/arxiv_scratcher/constant.py
@@ -1,5 +1,4 @@
-
 arxiv_prefix = "https://arxiv.org"
 arxiv_cs_cl_url = "https://arxiv.org/list/cs.CL/new"
 prefix_foler = "data/"
-instructions = '''\n请将上列文章进行分类,按照下面的格式\n### 类别名\n#### 文章标题|文章标题的中文翻译'''
+instructions = """\n请将上列文章进行分类,按照下面的格式\n### 类别名\n#### 文章标题|文章标题的中文翻译"""
